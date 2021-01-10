cfffc08c3cac45cc9ecb27fa3f2f097599aaefae
Dionei Bianchati <dion.biancha@gmail.com>
2021-01-10 15:26:17 -0300

    Remove header

--- src/navigators/MainStack.js
+++ src/navigators/MainStack.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import Welcome from '../screens/Welcome';
+import Home from '../screens/Home';
 
 const MainStack = createStackNavigator();
 
 export default () =>  ( 
-  <MainStack.Navigator>
+  <MainStack.Navigator screenOptions={{
+    headerShown: false
+  }}>
     <MainStack.Screen  name="Welcome" component={Welcome}/>
+    <MainStack.Screen  name="Home" component={Home}/>
   </MainStack.Navigator>
 );
--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import styled from 'styled-components/native'
+import { Button, View } from 'react-native';
+import styled from 'styled-components/native';
 
 const Page = styled.SafeAreaView`
   flex: 1;
@@ -9,12 +10,13 @@ const Page = styled.SafeAreaView`
 
 const Text = styled.Text`
   font-size: 20px;
+  
 `;
-
-export default () => {
+export default (props) => {
   return (
-    <Page>
-      <Text>Welcome Motherfuck</Text>
+    <Page onLoad={setTimeout(() => {props.navigation.navigate('Home')}, 5000)}>
+      <Text>Vacina salva vidas!</Text>
     </Page>
+    
   );
 }
